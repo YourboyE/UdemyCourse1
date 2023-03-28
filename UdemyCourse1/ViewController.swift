e8676d59d8928f5fe5a53a89130d251ea7ef7d3a
YourBoyE <egcrossmover@gmail.com>
2023-03-28 10:49:24 -0700

    Our first commit in Udemy Course

--- UdemyCourse1/ViewController.swift
+++ UdemyCourse1/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Variables
+    
+    var num = 0
+    var sum = 0
+    
+    
+    // Outlets
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
     }
 
+    
+    // Buttons
+    @IBAction func PressMeBtn(_ sender: Any) {
+        
+        num = num + 1
+        sum = num
+        
+        myLabel.text = "\(sum)"
+        view.backgroundColor = UIColor.white
+        
+        if sum >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "CONGRATS, THAT'S 10!"
+            num = 0
+        }
+    }
+    
+    
+    // Functions
+    
 
 }
 
